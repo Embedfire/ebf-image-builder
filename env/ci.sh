241edc8fa44aa05f336d274f276629374d14a78b
zhanqinghui <1442190496@qq.com>
2021-06-08 08:42:07 +0800

    目前只保留6ull的自动构建

--- env/ci.sh
+++ env/ci.sh
@@ -194,20 +194,20 @@ if [  $build_cpu ]; then
             imx6ull_build_img enable
             ;;
 
-        stm32mp157)
-            stm32mp157_build_img enable
-            ;;
+        #stm32mp157)
+        #    stm32mp157_build_img enable
+        #    ;;
 
-        rk3328)
-            rk3328_build_img enable
-            ;;  
+        #rk3328)
+        #    rk3328_build_img enable
+        #    ;;  
     esac
 
 else
     echo "只默认更新根文件系统"
     imx6ull_build_img
-    stm32mp157_build_img
-    rk3328_build_img
+    #stm32mp157_build_img
+    #rk3328_build_img
 fi
 
 
