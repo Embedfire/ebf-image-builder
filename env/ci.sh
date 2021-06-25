b5e640a979c9a9965ffb06f7d5cf0601baa24d34
zhanqinghui <1442190496@qq.com>
2021-06-25 13:38:59 +0800

    update

--- env/ci.sh
+++ env/ci.sh
@@ -228,7 +228,7 @@ end_time=`date +%s`
 time_cal $(($end_time - $start_time))
 
 rm -rf ${IMAGE_BUILDER_DIR}/deploy/
-rm -rf ${IMAGE_BUILDER_DIR}/ignore/
+#rm -rf ${IMAGE_BUILDER_DIR}/ignore/
 rm -rf ${IMAGE_BUILDER_DIR}/history/imx6ull/
 rm -rf ${IMAGE_BUILDER_DIR}/history/stm32mp157/
 rm -rf ${IMAGE_BUILDER_DIR}/history/rockchip-3328/
