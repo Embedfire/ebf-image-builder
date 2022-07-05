f9f616d249a7f7859ee6f75a5960abe800f3555b
Louis <1270384833@qq.com>
2022-07-05 17:45:46 +0800

    修改imx8mm uboot构建脚本

--- scripts/create_imx8mm_uboot_bin.sh
+++ scripts/create_imx8mm_uboot_bin.sh
@@ -8,9 +8,11 @@ if [ ! "$SCRIPT_URL" == "" ]; then
 fi
 cp -v ${BUILD}/${MUBOOT_FILE} ebf-imx-mkimage/iMX8M
 cp -v spl/${SPL_BUILD_FILE} ebf-imx-mkimage/iMX8M
-cp -v arch/arm/dts/${UBOOT_DTB} ebf-imx-mkimage/iMX8M
+cp -v arch/arm/dts/${UBOOT_DTB} ebf-imx-mkimage/iMX8M/imx8mm-ddr4-evk.dtb
 
 cd ebf-imx-mkimage
+
+make SOC=iMX8MM clean
 
 #make SOC=iMX8MM flash_spl_uboot
 make SOC=iMX8MM flash_ddr4_evk_no_hdmi
