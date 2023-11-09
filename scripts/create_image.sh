249fdfe32d2d897eaac97eedd9013afdc801b242
lilin <zgwinli555@163.com>
2023-11-09 08:52:35 +0000

    add log

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -36,6 +36,8 @@ mkdir -p history/${target_name}/${DISTRIBUTION}/${time}/kernel_deb
 mkdir -p history/${target_name}/${DISTRIBUTION}/${time}/rootfs/${DISTRIB_TYPE}
 mkdir -p history/tempdir/$(date +%Y-%m)/${DISTRIBUTION}/${DISTRIB_RELEASE}
 
+echo "Log: USER:${USER}"
+
 if [ "$USER" != 'root' ]; then
 	echo "Building rootfs stage requires root privileges, please enter your passowrd:"
 	read PASSWORD
